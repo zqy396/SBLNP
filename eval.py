fba914f942662fd0a6ee052515cd9a7922c60776
xiagao <65389322+zqy396@users.noreply.github.com>
2023-03-23 01:16:56 +0800

    Delete eval.py

--- eval.py
+++
@@ -1,7 +0,0 @@
-import slideflow as sf
-
-P = sf.load_project('/mnt/zqy_data/Slideflow/BLCA_LNM')
-
-model = '/mnt/zqy_data/Slideflow/BLCA_LNM/models/00000-label-HP0-kfold1/label-HP0-kfold1_epoch20.zip'
-
-P.evaluate(model=model,outcomes='label',filters={'dataset':['eval']})
